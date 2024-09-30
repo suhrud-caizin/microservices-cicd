93221168ec4fe163d7cc2d3cb848a82ec0f0b6e1
suhrud-caizin <suhrud@caizin.com>
2024-09-30 14:26:12 +0530

    enable eureka client!

--- catalogue-service/src/main/java/com/czn/catalogue_service/CatalogueServiceApplication.java
+++ catalogue-service/src/main/java/com/czn/catalogue_service/CatalogueServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CatalogueServiceApplication {
 
 	public static void main(String[] args) {
